07950e751896d00bfc1f8a52c0becab85ecbc50d
Leaksan <kanekiledoux@gmail.com>
2026-02-18 02:57:59 +0100

    fix ?

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis';
 const kv = new Redis({
-  url: process.env.KV_URL,
+  url: process.env.KV_REST_API_URL,
   token: process.env.KV_REST_API_TOKEN,
 });
 import { v4 as uuidv4 } from 'uuid';
